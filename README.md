3241d21986ee43dce210f0b380554be43d088a12
felipe1000 <felipeguedes01@hotmail.com>
2023-04-08 19:23:56 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Html5_e_Css3
+ Curso em Vídeo de Html e Css
---
+++ exercicios/ex012/tel.php
@@ -0,0 +1,14 @@
+<?php
+header('Location:"tel:+5551984899564"');
+?><!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu Telefone</title>
+</head>
+<body>
+    
+</body>
+</html>
